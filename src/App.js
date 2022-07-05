5feab9091f509de744bd3917651f0cb5aac94d5f
Jeffery <up937100@myport.ac.uk>
2022-07-06 00:22:41 +0100

    Added geolocation handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  function handlePermission(){
+    navigator.permissions.query({name:'geolocation'}).then(function(result) {
+      if (result.state === 'granted'){
+        console.log(result.state);
+        navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+      }else if(result.state === 'prompt'){
+        console.log(result.state);
+        navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+      }else if(result.state === 'denied'){
+        console.log(result.state);
+      }
+    })
+  }
+
+  function positionSuccess(position){
+    const coords = position.coords;
+
+    console.log(`Lat: ${coords.latitude}`);
+    console.log(`Long: ${coords.longitude}`);
+  }
+
+  function positionError(err){
+    console.warn(`Error: ${err.code} : ${err.message}`);
+  }
+
+  useEffect(() => {
+    handlePermission();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world
     </div>
   );
 }
